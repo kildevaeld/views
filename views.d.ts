e76190125fdaf889c4de1c77435ec7059000287f
Softshag & Me <admin@softshag.dk>
2015-07-26 13:58:17 +0200

    updated export

--- views.d.ts
+++ views.d.ts
@@ -1,6 +1,25 @@
 
-
 declare module 'views' {
-  import tmp = require('lib/index')
-  export = tmp
+  export * from 'lib/view'
+  export * from 'lib/templateview'
+  export * from 'lib/baseview'
+  export * from 'lib/utils'
+  export * from 'lib/events'
+  export * from 'lib/object'
+}
+
+declare module 'views/lib/view' {
+  export * from 'lib/view'
+}
+
+declare module 'views/lib/events' {
+  export * from 'lib/events'
+}
+
+declare module 'views/lib/utils' {
+  export * from 'lib/utils'
+}
+
+declare module 'views/lib/events' {
+  export * from 'lib/events'
 }
