53db074a8b8ca36fa0f06fa3219a4ca77f17dd3a
Softshag & Me <admin@softshag.dk>
2015-08-12 13:27:02 +0200

    bump

--- gulp/tasks/bump.js
+++ gulp/tasks/bump.js
@@ -6,10 +6,12 @@ const gulp = require('gulp'),
 			
 gulp.task('bump:patch', function () {
 	return gulp.src(['./package.json','bower.json'])
-	.pipe(bump());
+	.pipe(bump())
+	.pipe(gulp.dest('./'));
 });
 
 gulp.task('bump:minor', function () {
 	return gulp.src(['./package.json','bower.json'])
-	.pipe(bump({type:'minor'}));
+	.pipe(bump({type:'minor'}))
+	.pipe(gulp.dest('./'));
 });
